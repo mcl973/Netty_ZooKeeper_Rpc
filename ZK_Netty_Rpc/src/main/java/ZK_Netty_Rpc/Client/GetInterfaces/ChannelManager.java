a7639fc17a317693280dc478b9a3402e8dd85028
mcl <48353260+mcl973@users.noreply.github.com>
2020-07-07 16:28:40 +0800

    Create ChannelManager.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/GetInterfaces/ChannelManager.java
@@ -0,0 +1,21 @@
+package ZK_Netty_Rpc.Client.GetInterfaces;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ChannelManager {
+    public static ConcurrentHashMap<String,IPPort> ClientRpcIoc = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<String,String> IPPortToclass = new ConcurrentHashMap<>();
+
+    /**
+     * 封装ip、port、instance为IPPort作为value，使用classname作为key
+     * @param ipandport  ip:port 字符串
+     * @param classname  接口名
+     * @param instance   接口的实现类的代理类
+     */
+    public void SetAndGetSocket(String ipandport,String classname,Object instance){
+        String[] split = ipandport.split(":");
+        IPPort ipPort = new IPPort(Integer.parseInt(split[1]), split[0],instance);
+        ClientRpcIoc.put(classname,ipPort);
+        IPPortToclass.put(ipandport,classname);
+    }
+}
