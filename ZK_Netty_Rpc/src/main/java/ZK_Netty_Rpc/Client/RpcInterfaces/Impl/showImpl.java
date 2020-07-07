10346215c5327186f41826ee6b0b75250459a89b
mcl <48353260+mcl973@users.noreply.github.com>
2020-07-07 16:26:40 +0800

    Create showImpl.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/RpcInterfaces/Impl/showImpl.java
@@ -0,0 +1,9 @@
+package ZK_Netty_Rpc.Client.RpcInterfaces.Impl;
+import ZK_Netty_Rpc.Client.RpcInterfaces.show;
+
+
+public class showImpl implements show {
+    public  String showmore(){ return "0";}
+    public  int state(){ return 0;}
+    public void setname(String name){ return ;}
+}
