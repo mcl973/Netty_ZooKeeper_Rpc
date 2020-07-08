f06be26eda57c4b5c7e8c709d653fcc51f916833
mcl <48353260+mcl973@users.noreply.github.com>
2020-07-08 11:50:33 +0800

    Create MessageResult.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Info/MessageResult.java
@@ -0,0 +1,38 @@
+package ZK_Netty_Rpc.Info;
+
+import java.io.Serializable;
+
+public class MessageResult implements Serializable {
+    private long serialVersionUID = 1L;
+    private String classname;
+    private String methodname;
+    private Object result;
+
+    public MessageResult(String classname, String methodname, Object result) {
+        this.classname = classname;
+        this.methodname = methodname;
+        this.result = result;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public String getMethodname() {
+        return methodname;
+    }
+
+    public Object getObject() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageResult{" +
+                "serialVersionUID=" + serialVersionUID +
+                ", classname='" + classname + '\'' +
+                ", methodname='" + methodname + '\'' +
+                ", result=" + result +
+                '}';
+    }
+}
