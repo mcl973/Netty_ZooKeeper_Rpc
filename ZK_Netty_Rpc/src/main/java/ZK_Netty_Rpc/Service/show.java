302a114263ba756d47b5d9ab2d0649c26123665a
mcl <48353260+mcl973@users.noreply.github.com>
2020-07-08 11:54:16 +0800

    Create show.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Service/show.java
@@ -0,0 +1,15 @@
+package ZK_Netty_Rpc.Service;
+
+import ZK_Netty_Rpc.Extras.MyAnnonation.Export;
+
+/**
+ * 一个接口对应一个实现类，不可多个对用，主要是用来实现rpc的远程调用的
+ * 上面的规范仅限于这个借口里的方法，其他的接口不做规范
+ */
+@Export
+//#####
+public interface show {
+    public  String showmore();
+    public  int state();
+    public void setname(String name);
+}
