2fc2e1ad988f2344ff9dc0a00114624c3fbf5245
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-29 22:17:17 +0800

    Update show.java

--- ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Service/show.java
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Service/show.java
@@ -7,6 +7,7 @@ import ZK_Netty_Rpc.Extras.MyAnnonation.Export;
  * 上面的规范仅限于这个借口里的方法，其他的接口不做规范
  */
 @Export
+//下面的#####分隔符在argsInfo中有了明确的定义了，名为 FileSplit="#####"
 //#####
 public interface show {
     public  String showmore();
