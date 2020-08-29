b51bb2410fca98e25f59387850a6061ba7b5de2a
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-29 22:16:22 +0800

    Update ServerAllHandler.java

--- ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/ServerAllHandler.java
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/ServerAllHandler.java
@@ -1,15 +1,18 @@
 package ZK_Netty_Rpc.Server;
 
+import ZK_Netty_Rpc.Info.argsInfo;
 import ZK_Netty_Rpc.Server.DeEncoder.MyDecoder;
 import ZK_Netty_Rpc.Server.DeEncoder.MyEncoder;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 
 public class ServerAllHandler extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new DelimiterBasedFrameDecoder(argsInfo.FrameSplitSize,argsInfo.BasedFrame));
         pipeline.addLast(new MyEncoder());
         pipeline.addLast(new MyDecoder());
         pipeline.addLast(new ServerHandler());
