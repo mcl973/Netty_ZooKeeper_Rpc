4a3fae5e2a0fdabb04afe005d9ef615452913b58
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-29 22:14:36 +0800

    Update MyEncoder.java

--- ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MyEncoder.java
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MyEncoder.java
@@ -2,6 +2,7 @@ package ZK_Netty_Rpc.Server.DeEncoder;
 
 import ZK_Netty_Rpc.Info.MessageForNetty;
 import ZK_Netty_Rpc.Info.SerializableAndUnSerializable;
+import ZK_Netty_Rpc.Info.argsInfo;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
@@ -12,10 +13,10 @@ import io.netty.handler.codec.MessageToByteEncoder;
  * 将MessageForNetty序列化成字节数组
  */
 public class MyEncoder  extends MessageToByteEncoder<MessageForNetty> {
-
     @Override
     protected void encode(ChannelHandlerContext ctx, MessageForNetty msg, ByteBuf out) throws Exception {
         byte[] bytes = SerializableAndUnSerializable.StringToByte(msg);
         out.writeBytes(bytes);
+        out.writeBytes(argsInfo.FrameSplit.getBytes());
     }
 }
