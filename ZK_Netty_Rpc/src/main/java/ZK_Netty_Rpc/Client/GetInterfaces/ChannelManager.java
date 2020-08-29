d3e4ed70f5a01eaf83d44ff68cc5d46807e66410
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-29 22:05:39 +0800

    Update ChannelManager.java

--- ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/GetInterfaces/ChannelManager.java
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/GetInterfaces/ChannelManager.java
@@ -1,11 +1,17 @@
 package ZK_Netty_Rpc.Client.GetInterfaces;
 
+import ZK_Netty_Rpc.Client.RpcInterfaces.show;
+
+import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChannelManager {
     public static ConcurrentHashMap<String,IPPort> ClientRpcIoc = new ConcurrentHashMap<>();
     public static ConcurrentHashMap<String,String> IPPortToclass = new ConcurrentHashMap<>();
-
+    static String getclassname(String name){
+        String[] split = name.split("\\.");
+        return split[split.length-1];
+    }
     /**
      * 封装ip、port、instance为IPPort作为value，使用classname作为key
      * @param ipandport  ip:port 字符串
@@ -15,7 +21,8 @@ public class ChannelManager {
     public void SetAndGetSocket(String ipandport,String classname,Object instance){
         String[] split = ipandport.split(":");
         IPPort ipPort = new IPPort(Integer.parseInt(split[1]), split[0],instance);
+        System.out.println(classname);
         ClientRpcIoc.put(classname,ipPort);
-        IPPortToclass.put(ipandport,classname);
+//        IPPortToclass.put(ipandport,classname);
     }
 }
