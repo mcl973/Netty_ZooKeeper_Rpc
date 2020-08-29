7cd8877bacd77b5cff97a19d00762e9a65dd9799
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-29 22:04:01 +0800

    Create ConnectToServerPool.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/ConnectToServerPool/ConnectToServerPool.java
@@ -0,0 +1,27 @@
+package ZK_Netty_Rpc.Client.ConnectToServerPool;
+
+import ZK_Netty_Rpc.Client.ConnectToServerPool.NettyClient.Client;
+import ZK_Netty_Rpc.Extras.MyThreadPool;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ConnectToServerPool {
+    private static ConcurrentHashMap<String, Client> connectToServerPool = new ConcurrentHashMap<>();
+    private static ThreadPoolExecutor defaultThreadPoolExcutor = MyThreadPool.getDefaultThreadPoolExcutor();
+    public static void addOneConnectLink(String ipport){
+        String[] split = ipport.split(":");
+        addConnextLinkForIPAndPort(split[0],Integer.parseInt(split[1]));
+    }
+    public static void addConnextLinkForIPAndPort(String ip,int port){
+        Client client = new Client(ip, port);
+        connectToServerPool.put(ip+":"+port,client);
+        defaultThreadPoolExcutor.execute(client);
+    }
+    public static Client getConnectLink(String ipport){
+        return connectToServerPool.get(ipport);
+    }
+    public static Client getConnectForIPAndPort(String ip,int port){
+        return getConnectLink(ip+":"+port);
+    }
+}
