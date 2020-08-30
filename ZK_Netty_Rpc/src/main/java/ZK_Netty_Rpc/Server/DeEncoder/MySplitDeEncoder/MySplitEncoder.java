a7337eba6eea381e663c2e66628876f5b645c58a
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-30 22:43:57 +0800

    Update and rename ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MyEncoder.java to ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MySplitDeEncoder/MySplitEncoder.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MySplitDeEncoder/MySplitEncoder.java
@@ -0,0 +1,45 @@
+package ZK_Netty_Rpc.Server.DeEncoder;
+
+import ZK_Netty_Rpc.Info.MessageForNetty;
+import ZK_Netty_Rpc.Info.SerializableAndUnSerializable;
+import ZK_Netty_Rpc.Info.argsInfo;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+
+/**package ZK_Netty_Rpc.Server.DeEncoder.MySplitDeEncoder;
+
+import ZK_Netty_Rpc.Info.MessageForNetty;
+import ZK_Netty_Rpc.Info.MyDeEncoderProtocol;
+import ZK_Netty_Rpc.Info.SerializableAndUnSerializable;
+import ZK_Netty_Rpc.Info.argsInfo;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+
+/**
+ * 自定义编码器
+ * 将MessageForNetty序列化成字节数组
+ */
+public class MySplitEncoder extends MessageToByteEncoder<MyDeEncoderProtocol> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, MyDeEncoderProtocol msg, ByteBuf out) throws Exception {
+        byte[] bytes = SerializableAndUnSerializable.StringToByte(msg.getMessageForNetty());
+        out.writeBytes(bytes);
+        out.writeBytes(argsInfo.FrameSplit.getBytes());
+    }
+}
+
+ * 自定义编码器
+ * 将MessageForNetty序列化成字节数组
+ */
+public class MyEncoder  extends MessageToByteEncoder<MessageForNetty> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, MessageForNetty msg, ByteBuf out) throws Exception {
+        byte[] bytes = SerializableAndUnSerializable.StringToByte(msg);
+        out.writeBytes(bytes);
+        out.writeBytes(argsInfo.FrameSplit.getBytes());
+    }
+}
