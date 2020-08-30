935c338e769fc478094064d7602ba1d52e17e71f
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-30 22:43:17 +0800

    Update and rename ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MyDecoder.java to ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MySplitDeEncoder/MySplitDecoder.java

--- ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MySplitDeEncoder/MySplitDecoder.java
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Server/DeEncoder/MySplitDeEncoder/MySplitDecoder.java
@@ -1,6 +1,7 @@
-package ZK_Netty_Rpc.Server.DeEncoder;
+package ZK_Netty_Rpc.Server.DeEncoder.MySplitDeEncoder;
 
 import ZK_Netty_Rpc.Info.MessageForNetty;
+import ZK_Netty_Rpc.Info.MyDeEncoderProtocol;
 import ZK_Netty_Rpc.Info.SerializableAndUnSerializable;
 import com.sun.org.apache.xml.internal.serializer.utils.Utils;
 import io.netty.buffer.ByteBuf;
@@ -15,14 +16,13 @@ import java.util.List;
  * 自定义解码器
  * 将字节数组解码成MessageForNetty类
  */
-public class MyDecoder extends MessageToMessageDecoder<ByteBuf> {
-
+public class MySplitDecoder extends MessageToMessageDecoder<ByteBuf> {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
         byte[] array = new byte[msg.readableBytes()];
         msg.readBytes(array);
         MessageForNetty messageForNetty = (MessageForNetty)SerializableAndUnSerializable.ByteToString(array);
-        out.add(messageForNetty);
+        MyDeEncoderProtocol myDeEncoderProtocol = new MyDeEncoderProtocol(messageForNetty);
+        out.add(myDeEncoderProtocol);
     }
-
 }
