93d4f140344f49825314401e3be533a87c71f763
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-30 22:37:40 +0800

    Update and rename ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/ConnectToServerPool/DeEncoder/MyDecoder.java to ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/ConnectToServerPool/DeEncoder/MySplitDeEncoder/MySplitDecoder.java

--- ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/ConnectToServerPool/DeEncoder/MySplitDeEncoder/MySplitDecoder.java
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/ConnectToServerPool/DeEncoder/MySplitDeEncoder/MySplitDecoder.java
@@ -1,6 +1,7 @@
-package ZK_Netty_Rpc.Client.ConnectToServerPool.DeEncoder;
+package ZK_Netty_Rpc.Client.ConnectToServerPool.DeEncoder.MySplitDeEncoder;
 
 import ZK_Netty_Rpc.Info.MessageForNetty;
+import ZK_Netty_Rpc.Info.MyDeEncoderProtocol;
 import ZK_Netty_Rpc.Info.SerializableAndUnSerializable;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
@@ -12,13 +13,14 @@ import java.util.List;
  * 自定义解码器
  * 将字节数组解码成MessageForNetty类
  */
-public class MyDecoder extends MessageToMessageDecoder<ByteBuf> {
+public class MySplitDecoder extends MessageToMessageDecoder<ByteBuf> {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
         byte[] array = new byte[msg.readableBytes()];
         msg.readBytes(array);
         MessageForNetty messageForNetty = (MessageForNetty)SerializableAndUnSerializable.ByteToString(array);
+        MyDeEncoderProtocol myDeEncoderProtocol = new MyDeEncoderProtocol(messageForNetty);
         if (messageForNetty!=null)
-            out.add(messageForNetty);
+            out.add(myDeEncoderProtocol);
     }
 }
