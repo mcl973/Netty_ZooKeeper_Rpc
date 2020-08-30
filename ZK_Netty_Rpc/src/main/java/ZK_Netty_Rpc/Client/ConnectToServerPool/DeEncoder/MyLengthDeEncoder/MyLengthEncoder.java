e49d5b53d013535c3270850dbca41b3c2998eeda
mcl <48353260+mcl973@users.noreply.github.com>
2020-08-30 22:39:51 +0800

    Create MyLengthEncoder.java

---
+++ ZK_Netty_Rpc/src/main/java/ZK_Netty_Rpc/Client/ConnectToServerPool/DeEncoder/MyLengthDeEncoder/MyLengthEncoder.java
@@ -0,0 +1,17 @@
+package ZK_Netty_Rpc.Client.ConnectToServerPool.DeEncoder.MyLengthDeEncoder;
+
+import ZK_Netty_Rpc.Info.MyDeEncoderProtocol;
+import ZK_Netty_Rpc.Info.SerializableAndUnSerializable;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class MyLengthEncoder  extends MessageToByteEncoder<MyDeEncoderProtocol> {
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, MyDeEncoderProtocol myDeEncoderProtocol, ByteBuf byteBuf) throws Exception {
+        byte[] bytes = SerializableAndUnSerializable.StringToByte(myDeEncoderProtocol.getMessageForNetty());
+        int len = bytes.length;
+        byteBuf.writeInt(len);
+        byteBuf.writeBytes(bytes);
+    }
+}
